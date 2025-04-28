ab23f7be2207aa45989c4205d7b0b07bc54c3d1a
Tushank Bhardwaj <tushankbhardwaj121@gmail.com>
2025-04-29 00:24:28 +0530

    update in compose file

--- todo/database/database.go
+++ todo/database/database.go
@@ -1,17 +1,40 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+// func Connect() {
+// 	dsn := "host=localhost user=GO_DEV password=GO_DEV dbname=GO_DEV port=5433 sslmode=disable"
+// 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic("failed to connect to database.")
+// 	}
+
+// 	DB = database
+// }
+
 func Connect() {
-	dsn := "host=localhost user=GO_DEV password=GO_DEV dbname=GO_DEV port=5433 sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect to database.")
+		panic("failed to connect to database")
 	}
 
 	DB = database
